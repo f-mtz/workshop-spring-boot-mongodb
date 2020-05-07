7f6c76f773f146e33aea45984605a57bdc96a8a2
f-mtz <mtz.dev.si@gmail.com>
2020-05-07 13:32:43 -0300

    Usando padrao DTO para retornar usuarios

--- src/main/java/com/fmtz/workshopmongo/resources/UserResource.java
+++ src/main/java/com/fmtz/workshopmongo/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.fmtz.workshopmongo.resources;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fmtz.workshopmongo.domain.User;
+import com.fmtz.workshopmongo.dto.UserDTO;
 import com.fmtz.workshopmongo.services.UserService;
 
 @RestController
@@ -19,9 +21,10 @@ public class UserResource {
 	private UserService service;
 
 	@RequestMapping(method=RequestMethod.GET)/**Aqui valeria o @GetMapping p/ dizer q isso fará uma requisição do tipo GET*/
-	public ResponseEntity<List<User>> findAll(){
+	public ResponseEntity<List<UserDTO>> findAll(){
 		List<User> list = service.findAll();
-		return ResponseEntity.ok().body(list);
+		List<UserDTO> listDTO = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
 	}
 	
 	/**O Responseentity foi inserido depois, pois ele é uma entidade que permite fazer requisições HTTP porém personalizando
